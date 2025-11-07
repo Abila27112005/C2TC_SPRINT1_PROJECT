e4f70a67b1eb8d542a81559095b55592bedecba7
abila <abila@rizenlaptop>
2025-11-07 21:41:25 +0530

    Sprint1project

--- customerservice/src/main/java/com/tnsif/customerservice/configuration/AppConfig.java
+++ customerservice/src/main/java/com/tnsif/customerservice/configuration/AppConfig.java
@@ -1,8 +1,23 @@
 package com.tnsif.customerservice.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class AppConfig {
-    // Future custom configurations can be added here
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")  // React frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 }
